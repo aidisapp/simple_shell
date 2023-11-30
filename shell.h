c5f77747c9a6305928bb57d4496c662e79456b01
Prosper Atu <atu.prosper@gmail.com>
2023-11-30 21:25:47 +0100

    Updated header file

--- shell.h
+++ shell.h
@@ -4,10 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+int _print(char *str);
 
 #endif
 
