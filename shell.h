83f49f6839f183c282156d1077676fd58f0aeff5
Prosper Atu <atu.prosper@gmail.com>
2023-11-30 21:10:20 +0100

    added header file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#endif
+
