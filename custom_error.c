09e0acdfa6b6145f8ec46a729f935848d8367a0d
Prosper Atu <atu.prosper@gmail.com>
2023-12-15 04:33:42 -0800

    added handle cd error to custom print

--- custom_error.c
+++ custom_error.c
@@ -14,11 +14,10 @@ int custom_print(char *str)
 /**
  * print_string - write error not found message
  * @str: command input string
+ * @len: length of string
  */
-void print_string(char *str)
+void print_string(char *str, int len)
 {
-	int len = strlen(str);
-
 	write(STDOUT_FILENO, str, len);
 }
 
@@ -44,16 +43,52 @@ void err_not_found(char *cmd, int line_num, list_t *env_list)
 	char *shell_name = get_shell_name(env_list);
 	char *cmd_num_str = int_to_string(line_num);
 
-	print_string(shell_name);
-	print_string(": ");
+	custom_print(shell_name);
+	custom_print(": ");
 
-	print_string(cmd_num_str);
-	print_string(": ");
+	custom_print(cmd_num_str);
+	custom_print(": ");
 
-	print_string(cmd);
-	print_string(": not found\n");
+	custom_print(cmd);
+	custom_print(": not found\n");
 
 	free(shell_name);
 	free(cmd_num_str);
+}
+
+/**
+ * handle_cd_error - Prints error message for failed cd command
+ * @dir: Invalid directory path attempted
+ * @cmd_count: Current command count
+ * @env: Environment vars list
+ */
+void handle_cd_error(char *dir, int cmd_count, list_t *env)
+{
+	int count = 0;
+	char *shell_name = get_shell_name(env), *num;
+
+	while (shell_name[count] != '\0')
+		count++;
+
+	print_string(shell_name, count);
+	free(shell_name);
+	custom_print(": ");
+	num = int_to_string(cmd_count);
+	count = 0;
+
+	while (num[count] != '\0')
+		count++;
+
+	print_string(num, count);
+	free(num);
+	custom_print(": ");
+	custom_print("cd: can't cd to ");
+	count = 0;
+
+	while (dir[count] != '\0')
+		count++;
+
+	print_string(dir, count);
+	custom_print("\n");
 }
 
